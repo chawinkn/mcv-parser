dc06e7af018c36c03406c70cc7731aa648e4d1ff
chawinkn <chawin26112549@gmail.com>
2026-05-21 00:34:42 +0700

    feat: add argparse login

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import argparse
+import getpass
 from mcv import MCVParser
 
 if __name__ == "__main__":
-	student_id = input("Student ID (10 digits): ")
-	password = input("Password: ")
-	mcvp = MCVParser(student_id, password)
+    parser = argparse.ArgumentParser(description="MCV Parser login")
+    parser.add_argument("-u", "-s", "--username", "--student-id", help="Student ID (10 digits)", dest="username")
+    parser.add_argument("-p", "--password", help="Password")
+    args = parser.parse_args()
 
-	if mcvp.login():
-		mcvp.dump_materials()
-	else:
-		print("Incorrect username or password")
+    username = args.username or input("Student ID (10 digits): ")
+    password = args.password or getpass.getpass("Password: ")
+
+    mcvp = MCVParser(username, password)
+
+    if mcvp.login():
+        mcvp.dump_materials()
+    else:
+        print("Incorrect username or password")
